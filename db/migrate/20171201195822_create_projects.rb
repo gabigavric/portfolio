846fc262c9ed30831c500938233bf8285b3e8c4a
gabrijela <gabigavric@gmail.com>
2017-12-01 12:14:09 -0800

    add name image and about to database

--- db/migrate/20171201195822_create_projects.rb
+++ db/migrate/20171201195822_create_projects.rb
@@ -1,6 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+      t.column :title, :string
+      t.column :image, :string
+      t.column :about, :text
     end
   end
 end

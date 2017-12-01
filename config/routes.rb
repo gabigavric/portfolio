da6e5c551e3f8999717665ff59190c0e5ecfd137
gabrijela <gabigavric@gmail.com>
2017-12-01 13:27:29 -0800

    made a page to add a navbar still dont know how to add just admin authentication and no user

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :projects
-  # devise_for :usersend
-
-  # make welcome landing page
   root :to => 'welcome#index'
-
-  # resources :welcome do
-  #   resources :something?
-  # end
-  #
-  # resources :projects do
-  #   resources :something?
-  # end
-
-  # figure out snippet below
-  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # devise_for :users
+  resources :welcome, :only => :index
 end

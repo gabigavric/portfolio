bbf34b1dccf27fc0e0a9c754fe9f73c0ee7a472f
gabrijela gavric <gabigavric@gmail.com>
2017-12-05 14:27:33 -0800

    add missing config route take out line that is throwing error and make empty controllers for landing page called welcome and projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,3 @@
 class ProjectsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  def create
-    @project = current_admin.projects.new(project_params)
-    if @project.save
-      redirect_to root_path
-    end
-  end
 
-  private
-    def project_params
-      params.permit([:title, :image, :about])
-    end
 end

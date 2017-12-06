fb92caf4e59133c42b6de18a87709b981263f76e
Guest User <Guest@Epicodus-1E.local>
2017-12-06 10:32:54 -0800

    add sass to application

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,3 @@ class User < ApplicationRecord
       false
   end
 end
-
-
-# class Ball < ActiveRecord::Base
-#   validates :name, presence: true
-#   belongs_to :player
-#
-#   rails_admin do
-#     configure :player do
-#       label 'Owner of this ball: '
-#     end
-#   end
-# end

ad5e9a31c6a2b7538cb3a44031141a5b3f644b78
Guest User <Guest@Epicodus-1E.local>
2017-12-06 11:30:05 -0800

    made basic and empty project controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,21 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+  end
+  def new
+    @project = project.new(params[:id])
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+
+  def project_params
   end
 end
